62bef4af30b1d71fcbb37b58a2b87da9beea62f0
Amarjeet <amarjeetmistri41@gmail.com>
2025-09-28 22:25:29 +0530

    update

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://rt-chat-79zw.vercel.app/"],
+    origin: ["http://localhost:5173", "https://rt-chat-79zw.vercel.app"],
     credentials: true,
   })
 );
