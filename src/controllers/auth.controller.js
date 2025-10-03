a54dc678c92a48108151e48a5678c65dcd4a0e05
Amarjeet <amarjeetmistri41@gmail.com>
2025-10-03 12:14:39 +0530

    fixed cookie error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,23 +4,7 @@ import bcrypt from "bcryptjs";
 import cloudinary from "../lib/cloudinary.js";
 
 
-import jwt from "jsonwebtoken";
 
-export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-
-  // cookie set karna yahin kar lo
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
-
-  return token;
-};
 
 
 
